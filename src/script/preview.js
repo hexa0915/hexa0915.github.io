1c2aec03431afd17247d6de16138a2c124e512d0
컨트롤D <controld3785@gmail.com>
2023-01-18 22:58:11 +0900

    Use YouTube IFrame API

--- src/script/preview.js
+++ src/script/preview.js
@@ -1,6 +1,50 @@
+let youtubeAPI = document.createElement("script")
+youtubeAPI.src = 'https://www.youtube.com/iframe_api'
+
+var firstScript = document.getElementsByTagName('script')[0];
+firstScript.parentNode.insertBefore(youtubeAPI, firstScript);
+
+let previewPlayer;
+function onYouTubePlayerAPIReady() {
+    previewPlayer = new YT.Player('preview', {
+        width: '1280',
+        height: '720',
+        videoId: 'nUrYVH4BE2A',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onStateChange
+        },
+        playerVars: {
+            controls: 0,
+            disablekb: 1,
+            mute: 1,
+            showInfo: 0,
+            modestbranding: 1,
+            playlist: 'nUrYVH4BE2A',
+            rel: 0,
+            loop: 1
+        }
+    })
+}
+
+function onPlayerReady(event) {
+    event.target.setPlaybackQuality('hd720')
+    event.target.playVideo()
+}
+
+function onStateChange(event) {
+    if (event.data == YT.PlayerState.ENDED) {
+        document.querySelector('#preview-cover').style.backgroundColor = "rgba(0, 0, 0, 1)"
+        previewPlayer.loadVideoById('nUrYVH4BE2A', 0, 'hd720')
+        previewPlayer.seekTo(5.67)
+        document.querySelector('#preview-cover').style.backgroundColor = "rgba(0, 0, 0, 0.2)"
+    }
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
-    let preview = document.querySelector('.preview')
-    let previewCover = document.querySelector('.preview-cover')
+    let previewCover = document.querySelector('#preview-cover')
+    let previewVideo = previewCover.querySelector('#preview')
+
     const previewLoop = () => {
         let previewWidth = window.innerWidth
         let previewHeight = previewWidth / 16 * 9
