370c61bfc8f2f6e3ce0bc48e80801a761521345b
컨트롤D <controld3785@gmail.com>
2023-01-19 11:18:57 +0900

    Fix Video Autoplay

--- src/script/preview.js
+++ src/script/preview.js
@@ -1,7 +1,3 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     let previewVideo = document.querySelector('#preview')
-
-    previewVideo.addEventListener("load", (event) => {
-        previewVideo.play()
-    })
 })
