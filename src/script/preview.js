e9245a130b264ec91c7873c16cc9f7ff4870702a
컨트롤D <controld3785@gmail.com>
2023-01-19 11:09:01 +0900

    Fix Video Play Error

--- src/script/preview.js
+++ src/script/preview.js
@@ -1,13 +1,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    let previewCover = document.querySelector('#preview-cover')
-    let previewVideo = previewCover.querySelector('#preview')
+    let previewVideo = document.querySelector('#preview')
 
-    const previewLoop = () => {
-        let previewWidth = window.innerWidth
-        let previewHeight = previewWidth / 16 * 9
-
-        requestAnimationFrame(previewLoop)
-    }
-
-    requestAnimationFrame(previewLoop)
+    previewVideo.addEventListener("load", (event) => {
+        previewVideo.play()
+    })
 })
