88cc5438615d95bfc640bc94d2acf9c71b9d8015
컨트롤D <controld3785@gmail.com>
2023-01-19 17:25:24 +0900

    Fix Preview Video Size in Mobile

--- src/script/preview.js
+++ src/script/preview.js
@@ -3,19 +3,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
     let previewCover = document.querySelector('#preview-cover')
 
     const previewSize = () => {
-        if (!isMobile) {
-            let preview_width = window.innerWidth
-            let preview_height = window.innerHeight
-            if (preview_width / 16 * 9 > preview_height) { //세로 레터박스 생김
-                previewVideo.style.transform = 
-                    `scale(${(preview_width / 16 * 9) / preview_height})`
-                previewCover.style.transform = 
-                    `scale(${(preview_width / 16 * 9) / preview_height})`
-            } else if (preview_width / 16 * 9 < preview_height) { //가로 레터박스 생김
-                previewVideo.style.transform = `scale(1)`
-                previewCover.style.transform = `scale(1)`
-            }
-        } else {
+        let preview_width = window.innerWidth
+        let preview_height = window.innerHeight
+        if (preview_width / 16 * 9 > preview_height) { //세로 레터박스 생김
+            previewVideo.style.transform = 
+                `scale(${(preview_width / 16 * 9) / preview_height})`
+            previewCover.style.transform = 
+                `scale(${(preview_width / 16 * 9) / preview_height})`
+        } else if (preview_width / 16 * 9 < preview_height) { //가로 레터박스 생김
+            previewVideo.style.transform = `scale(1)`
+            previewCover.style.transform = `scale(1)`
+        }
+        
+        if (isMobile) {
             previewCover.style.opacity = 0.2
         }
 
